ec73a396b88cad092ca47bc4f8c0f5c68d6a9164
sim-sal <simone.saladino93@gmail.com>
2023-05-17 04:01:44 +0200

    added structure in DOM, like button, not like i+

---
+++ js/main.js
@@ -0,0 +1,171 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        likes: 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+
+// dichiaro variabile che associo al div presente nel DOM
+let postContainer = document.querySelector(".posts-list");
+// console.log(postContainer);
+
+// cicliamo il nostro array
+var totLikes = [];
+
+posts.forEach((element, index) => {
+
+    // console.log(element, index);
+
+    // stampiamo struttura nel div tramite innerHTML
+    postContainer.innerHTML += `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="${element.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+            <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid=${element.id} >
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${element.id} class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
+    const totLike = element.likes;
+    console.log(totLike);
+    totLikes.push(totLike);
+});
+
+console.log(totLikes);
+
+
+
+// selezioniamo tutti i bottoni per poi cambiarne il colore della scritta e incrementare il numero di like
+const bottoni = document.querySelectorAll(".js-like-button");
+// console.log(bottoni);
+
+// genero array vuoto per i post ai quali verrà associato il like
+let likesArray = [];
+
+
+
+// ciclo il mio simil array dei bottoni
+bottoni.forEach((element) => {
+
+    console.log(element);
+
+    // dichiaro la variabile per il conteggio dei post tramite l'id
+    // const counter = element.getAttribute("data-postid");
+    // console.log(counter);
+
+    // // creo il counter
+    // const likeCounter = document.getElementById(`like-counter-${counter}`);
+    
+    // // creo oggetto con proprietà id da aggiungere al likesArray ad ogni click sul like
+    // let newObj = {id:counter};
+
+    // gestione dell'evento al click
+    element.addEventListener("click",
+
+        function(){
+            if (element.classList.contains("like-button--liked")) {
+                element.classList.remove("like-button--liked");
+
+                const likes = totLikes + 1;
+                console.log(likes);
+
+                const counter = document.querySelector(".likes__counter");
+                counter.innerHTML = `Piace a <b id="like-counter-${element.id} class="js-likes-counter">${likes}</b> persone`
+
+
+            }else{
+                const likes = totLikes + 2;
+                console.log(likes);
+
+                element.classList.add("like-button--liked");
+                const counter = document.querySelector(".likes__counter");
+                counter.innerHTML = `Piace a <b id="like-counter-${element.id} class="js-likes-counter">${likes}</b> persone`
+            }
+        }
+
+    )
+
+    
+
+
+
+
+
+
+
+
+})
